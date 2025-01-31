d43389cec3c8fdd2151c40b21ee0afd903f76657
arsalan4work <you@example.com>
2025-01-31 21:47:54 +0500

    updated files

--- README.md
+++ README.md
@@ -1,36 +1,14 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+# Muhammad Arsalan's Portfolio
 
-## Getting Started
+**Date**: 30-1-2025
 
-First, run the development server:
+## About This Project
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+This is a static portfolio website made with **Aceternity UI**, **Shadcn**, **Framer Motion**, **Next.js**, **React.js**, and **Tailwind CSS**. The website is built in **Next.js 14** and is fully responsive, showcasing my work and projects.
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Links
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+- **GitHub Profile**: [Muhammad Arsalan GitHub](https://github.com/arsalan4work/arsalan-portfolio-website)
+- **Vercel Link**: [Live Portfolio](https://arsalan-portfolio-website-swart.vercel.app/)
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+Feel free to explore and connect!
---
+++ src/data/index.ts
@@ -0,0 +1,222 @@
+import { link } from "fs";
+
+export const navItems = [
+   { name: "About", link: "#about" },
+   { name: "Projects", link: "#projects" },
+   { name: "Testimonials", link: "#testimonials" },
+   { name: "Contact", link: "#contact" },
+ ];
+ 
+ export const gridItems = [
+   {
+     id: 1,
+     title: "I prioritize client collaboration, fostering open communication ",
+     description: "",
+     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+     imgClassName: "w-full h-full",
+     titleClassName: "justify-end",
+     img: "/b1.svg",
+     spareImg: "",
+   },
+   {
+     id: 2,
+     title: "I'm very flexible with time zone communications",
+     description: "",
+     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+     imgClassName: "",
+     titleClassName: "justify-start",
+     img: "",
+     spareImg: "",
+   },
+   {
+     id: 3,
+     title: "My tech stack",
+     description: "I constantly try to improve",
+     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+     imgClassName: "",
+     titleClassName: "justify-center",
+     img: "",
+     spareImg: "",
+   },
+   {
+     id: 4,
+     title: "Tech enthusiast with a passion for development.",
+     description: "",
+     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+     imgClassName: "",
+     titleClassName: "justify-start",
+     img: "/grid.svg",
+     spareImg: "/b4.svg",
+   },
+ 
+   {
+     id: 5,
+     title: "Currently learning Agentic AI and Machine Learning",
+     description: "Future Plans",
+     className: "md:col-span-3 md:row-span-2",
+     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+     titleClassName: "justify-center md:justify-start lg:justify-center",
+     img: "/b5.svg",
+     spareImg: "/grid.svg",
+   },
+   {
+     id: 6,
+     title: "Do you want to start a project together? Copy the email below 👇",
+     description: "",
+     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+     imgClassName: "",
+     titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+     img: "",
+     spareImg: "",
+   },
+ ];
+ 
+ export const projects = [
+   {
+     id: 1,
+     title: "Ecommerce Web Application",
+     des: "Explore the e-commerce application to grow your online business.",
+     img: "/p1.png",
+     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/fm.svg"],
+     link: "https://arsalan-ecommerce-website-hackaton.vercel.app/",
+   },
+   {
+     id: 2,
+     title: "Yoom - Video Conferencing App",
+     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+     img: "/p2.svg",
+     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+     link: "/ui.yoom.com",
+   },
+   {
+     id: 3,
+     title: "AI Image SaaS - Canva Application",
+     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+     img: "/p3.svg",
+     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+     link: "/ui.aiimg.com",
+   },
+   {
+     id: 4,
+     title: "Animated Apple Iphone 3D Website",
+     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+     img: "/p4.svg",
+     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+     link: "/ui.apple.com",
+   },
+ ];
+ 
+ export const testimonials = [
+  {
+    quote:
+      "Arsalan transformed our outdated website into a modern, user-friendly platform that perfectly represents our brand. His attention to detail and creative approach made the entire process seamless. Highly recommend his services!",
+    name: "Lisa Reynolds",
+    title: "Founder of FT.COM",
+  },
+  {
+    quote:
+      "We needed a fast and reliable e-commerce solution, and Arsalan delivered beyond expectations. His technical expertise and problem-solving skills helped us launch ahead of schedule. A true professional!",
+    name: "Jason Carter",
+    title: "CEO of Urban Threads",
+  },
+  {
+    quote:
+      "Arsalan was a pleasure to work with—responsive, skilled, and committed to excellence. Our startup’s website is now more engaging and optimized for conversions, thanks to his expertise.",
+    name: "Samantha Lopez",
+    title: "Marketing Director at FreshBites",
+  },
+  {
+    quote:
+      "Finding a developer who truly understands our needs was a challenge until we met Arsalan. His ability to translate our ideas into a functional, elegant website has been a game-changer for our business.",
+    name: "Mark Davies",
+    title: "Co-Founder of TechNest Solutions",
+  },
+  {
+    quote:
+      "Arsalan's work speaks for itself. He created a sleek, high-performing website for our creative agency, helping us attract more clients. His professionalism and skill set make him an invaluable asset to any project.",
+    name: "Olivia Chen",
+    title: "Creative Director at Pixel Studio",
+  },
+];
+ 
+ export const companies = [
+   {
+     id: 1,
+     name: "cloudinary",
+     img: "/cloud.svg",
+     nameImg: "/cloudName.svg",
+   },
+   {
+     id: 2,
+     name: "appwrite",
+     img: "/app.svg",
+     nameImg: "/appName.svg",
+   },
+   {
+     id: 3,
+     name: "HOSTINGER",
+     img: "/host.svg",
+     nameImg: "/hostName.svg",
+   },
+   {
+     id: 4,
+     name: "stream",
+     img: "/s.svg",
+     nameImg: "/streamName.svg",
+   },
+   {
+     id: 5,
+     name: "docker.",
+     img: "/dock.svg",
+     nameImg: "/dockerName.svg",
+   },
+ ];
+ 
+ export const workExperience = [
+   {
+     id: 1,
+     title: "Frontend Engineer Intern - Imerge Solutions",
+     desc: "Assisted in the development of a web-based platform using Next.js, enhancing interactivity.",
+     className: "md:col-span-2",
+     thumbnail: "/exp1.svg",
+   },
+   {
+     id: 2,
+     title: "Backend Developer Intern - Imerge Solutions",
+     desc: "Learning mobile app development for both iOS & Android platforms using React Native.",
+     className: "md:col-span-2", // change to md:col-span-2
+     thumbnail: "/exp2.svg",
+   },
+   {
+     id: 3,
+     title: "Freelance Web App Dev Project",
+     desc: "Led the dev of a web app for a client, from initial concept to deployment hostinger.",
+     className: "md:col-span-2", // change to md:col-span-2
+     thumbnail: "/exp3.svg",
+   },
+   {
+     id: 4,
+     title: "Bug Fixer Frontend Developer",
+     desc: "Re-Developed and maintained user-facing features and remove bugs.",
+     className: "md:col-span-2",
+     thumbnail: "/exp4.svg",
+   },
+ ];
+ 
+ export const socialMedia = [
+   {
+     id: 1,
+     img: "/git.svg",
+     link: "https://www.github.com/arsalan4work"
+   },
+   {
+     id: 2,
+     img: "/twit.svg",
+     link: "https://www.linkedin.com/in/muhammad-arsalan-ai-enthusiast/"
+   },
+   {
+     id: 3,
+     img: "/link.svg",
+     link: "https://www.linkedin.com/in/muhammad-arsalan-ai-enthusiast/"
+   },
+ ];
